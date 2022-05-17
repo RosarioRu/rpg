e935f8bfb8e9db6f295118b04925ccfc6c34d981
RosarioRu <mariarruval@gmail.com>
2022-05-16 19:26:12 -0700

    Add and pass class Villain with initial test for creating an instance of constructor.
    co-authored-by: Alexander Kirkpatrick <knibz@protonmail.com>
    Co-authored-by: Megan McKissack <meganmckissack@gmail.com>

---
+++ __tests__/villain.test.js
@@ -0,0 +1,12 @@
+import { Villain } from "../src/villain";
+
+
+describe('Villain', () => {
+  test('it should create a Villain character with 4 character properties', () => {
+    const villain = new Villain (4, 4, 4, 4);
+    expect(villain.cunning).toEqual(4);
+    expect(villain.snark).toEqual(4);
+    expect(villain.deceit).toEqual(4);
+    expect(villain.resilience).toEqual(4);
+  });
+});
--- src/villain.js
+++ src/villain.js
@@ -1,8 +1,8 @@
-// export class Villain {
-//   constructor (cunning, snark, deceit, resilience) {
-//     this.cunning = cunning;
-//     this.snark = snark;
-//     this.deceit = deceit;
-//     this.resilience = resilience;
-//   }
-// }
+export class Villain {
+  constructor (cunning, snark, deceit, resilience) {
+    this.cunning = cunning;
+    this.snark = snark;
+    this.deceit = deceit;
+    this.resilience = resilience;
+  }
+}

944b790482a5344107c7a83534632a1082e36984
RosarioRu <mariarruval@gmail.com>
2022-05-16 19:15:30 -0700

    Add js and test files for villain characters.
    co-authored-by: Alexander Kirkpatrick <knibz@protonmail.com>
    Co-authored-by: Megan McKissack <meganmckissack@gmail.com>

---
+++ src/villain.js
@@ -0,0 +1,8 @@
+// export class Villain {
+//   constructor (cunning, snark, deceit, resilience) {
+//     this.cunning = cunning;
+//     this.snark = snark;
+//     this.deceit = deceit;
+//     this.resilience = resilience;
+//   }
+// }
